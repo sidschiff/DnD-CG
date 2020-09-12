fbe53b1efaaf8e428397a8c45d78787416ae1d4a
Sid <sidneyschiff@gmail.com>
2020-09-12 09:20:33 -0600

    Adds basic server functionality for dev

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// app.get()
+
+app.listen(PORT, () => {
+  console.log(`server is running and listening on port ${PORT}`)
+});
