9ee08d08930fd5dfb85f6c2ce27bc5d2feafab78
Sid <sidneyschiff@gmail.com>
2020-09-12 08:54:08 -0600

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+const DIST_DIR = path.resolve(__dirname, 'client/dist');
+const SRC_DIR = path.resolve(__dirname, 'client/src');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: `${DIST_DIR}`,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.jsx$/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        },
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
